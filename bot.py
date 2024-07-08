35e926f3827630440afff5c5496ce1d09e1c7c85
Manu1wybggsn <saurabh112dwivedi@gmail.com>
2024-07-08 19:20:15 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+from plugins.config import Config
+
+from pyrogram import Client as Ntbots
+from pyrogram import filters
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+if __name__ == "__main__" :
+
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    plugins = dict(root="plugins")
+    Ntbots = Ntbots(
+        "URL UPLOADER BOT",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins)
+
+    print("🎊 I AM ALIVE 🎊  • Support @NT_BOTS_SUPPORT")
+    Ntbots.run()
+
